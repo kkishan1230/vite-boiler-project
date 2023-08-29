f1b0ef8a03a2d8cf83a8de7a481e743650ce6960
kkishan1230 <kkishan1230@gmail.com>
2023-08-29 10:57:38 +0530

    project cleanups

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { headingStyles as styles } from "./styling";
+import {} from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <h1 css={[styles.heading]}>kishan</h1>;
+  return <h1>kishan</h1>;
 }
 
 export default App;
--- src/styling.js
+++
@@ -1,8 +0,0 @@
-import tw, { css } from "twin.macro";
-
-export const headingStyles = {
-  heading: css`
-    ${tw`text-base`}
-    color: red;
-  `,
-};
